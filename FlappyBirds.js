df95c52010128a7d24d258d14efb1c8fb178cf4f
trisheenahepting <manderyth@yahoo.com>
2016-06-15 10:32:29 -0600

    6/14/16 10:32am

--- FlappyBirds.js
+++ FlappyBirds.js
@@ -1,13 +1,107 @@
+var pony;
+var rainbow;
+var currentState;
+var width;
+var height;
+var renderingContext;
+
+var states = {
+    Splash: 0,
+    Game: 1,
+    Score: 2
+};
+
 function main() {
-    windowSetup();
-    canvasSetup();
+    windowSetup(); //sets width and height
+    canvasSetup(); //sets up the canvas size
+    currentState = states.Splash; 
+    document.body.appendChild(canvas);
 
-    currentState = states.Splash; // Game begins at the splash screen.
-
-    document.body.appendChild(canvas); // Append the canvas we've created to the body element in our HTML document.
-
-    fish = new Fish();
-    corals = new CoralCollection();
+    pony = new Pony();
+    //rainbow = new RainbowCollection();
 
     loadGraphics();
+}
+
+function Pony() {
+    this.x = 140;
+    this.y =0;
+}
+
+
+
+function loadGraphics() {
+    var img = new Image();
+    img.src = "images/randbowDash.png";
+    img.onload = function () {
+        initSprites(this);
+        renderingContext.fillStyle = "Black";
+        renderingContext.fillRect(0, 0, width, height);
+        //backgroundsprite.draw(renderingContext, 0, height - backgroundsprite.height);
+        //backgroundsprite.draw(renderingContext, backgroundsprite.width - backgroundsprite.height);
+        ponysprite[0].draw(renderingContext, 5, 5, 142, 50);
+
+        /*okButton = {
+            x: (width - okButtonSprite.width) / 2,
+            y: height - 200,
+            width: okButtonSprite.width,
+            height: okButtonSprite.height
+        };*/
+
+        //gameLoop();
+    };
+}
+
+function windowSetup(){
+    width = window.innerWidth;
+    height = window.innerHeight;
+    var inputEvent = "touchstart";
+
+    if (width >= 500) {
+        width = 380;
+        height = 430;
+        inputEvent = "mousedown";
+    }
+    document.addEventListener(inputEvent, onpress);
+}
+
+function canvasSetup(){
+    canvas = document.createElement("canvas");
+    canvas.style.border = "15px solid #FFC0CB";
+    canvas.width = width;
+    canvas.height = height;
+    renderingContext = canvas.getContext("2d");
+}
+
+function onpress(evt) {
+    switch (currentState) {
+
+        case states.Splash: // Start the game and update the sprite speed.
+            currentState = states.Game;
+            pony.jump();
+            break;
+
+        case states.Game: // The game is in progress. Update sprite speed.
+            pony.jump();
+            break;
+
+        case states.Score: // Change from score to splash state if event within okButton bounding box
+            var mouseX = evt.offsetX, mouseY = evt.offsetY;
+
+            if (mouseX == null || mouseY == null) {
+                mouseX = evt.touches[0].clientX;
+                mouseY = evt.touches[0].clientY;
+            }
+
+            // Check if within the okButton
+            if (okButton.x < mouseX && mouseX < okButton.x + okButton.width &&
+                okButton.y < mouseY && mouseY < okButton.y + okButton.height
+            ) {
+                //console.log('click');
+                rainbow.reset();
+                currentState = states.Splash;
+                score = 0;
+            }
+            break;
+    }
 }
--- FlappyBirdsSprite.js
+++ FlappyBirdsSprite.js
@@ -1,1 +1,38 @@
+var
+    ponysprite,
+    toprainbowsprite,
+    bottomrainbowsprite,
+    backgroundsprite;
 
+function Sprite(img, x, y, width, height) {
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+}
+
+Sprite.prototype.draw = function (renderingContext, x, y) {
+    renderingContext.drawImage(this.img, this.x, this.y, this.width, this.height,
+        x, y, this.width, this.height);
+};
+
+function initSprites(img) {
+      ponysprite = [
+          new Sprite (img, 3, 1, 100, 44), //x, y, width, height
+          new Sprite (img, 103, 1, 100, 44), //x is horizontal, y is vertical
+          new Sprite (img, 203, 1, 100, 44),
+          new Sprite (img, 303, 1, 100, 44),
+      ]  
+    };
+
+
+/* old sprite sheet
+function initSprites(img) {
+    ponysprite = [
+        new Sprite (img, 5, 45, 42, 28), //x, y, width, height
+        new Sprite (img, 50, 45, 42, 28), //x is horizontal, y is vertical
+        new Sprite (img, 95, 45, 42, 28),
+        new Sprite (img, 140, 45, 42, 28),
+    ]
+};*/
