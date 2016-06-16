eeff1d5edd8cb8078612e69e6fb9857f68115d96
trisheenahepting <manderyth@yahoo.com>
2016-06-16 10:13:50 -0600

    6/15/16 10:13am

--- FlappyBirds.js
+++ FlappyBirds.js
@@ -4,6 +4,7 @@ var currentState;
 var width;
 var height;
 var renderingContext;
+var frames = 0;
 
 var states = {
     Splash: 0,
@@ -25,30 +26,64 @@ function main() {
 
 function Pony() {
     this.x = 140;
-    this.y =0;
-}
+    this.y = 200;
+
+    this.frame = 0;
+    this.velocity = 0;
+    this.animation = [3,2,1,0];
+
+    this.rotation = 0;
+    this.radius = 12;
+
+    this.gravity = 0.25;
+    this._jump = 4.6;
+
+    this.update = function () {
+        var n = currentState === states.Splash ? 10 : 5;
+
+        this.frame += frames % n === 0 ? 1 : 0;
+        this.frame %= this.animation.length;
+
+        if (currentState === states.Splash) {
+            this.updateIdlePony();
+        } else { // Game state
+            this.updatePlayingFish();
+        }
+    };
+
+
+
+    this.updateIdlePony = function () {
+        this.y = height - 280 + 5 * Math.cos(frames / 10);
+        this.rotation = 0;
+    };
+
+    this.draw = function (renderingContext) {
+            renderingContext.save();
+
+            renderingContext.translate(this.x, this.y);
+            renderingContext.rotate(this.rotation);
+
+            var n = this.animation[this.frame];
+
+            ponysprite[n].draw(renderingContext, -ponysprite[n].width / 2, -ponysprite[n].height / 2);
+
+            renderingContext.restore();
+        };
+    }
 
 
 
 function loadGraphics() {
     var img = new Image();
-    img.src = "images/randbowDash.png";
+    img.src = "images/randbowDash_newLegs.png";
     img.onload = function () {
         initSprites(this);
         renderingContext.fillStyle = "Black";
         renderingContext.fillRect(0, 0, width, height);
-        //backgroundsprite.draw(renderingContext, 0, height - backgroundsprite.height);
-        //backgroundsprite.draw(renderingContext, backgroundsprite.width - backgroundsprite.height);
-        ponysprite[0].draw(renderingContext, 5, 5, 142, 50);
+        //ponysprite[0].draw(renderingContext, 120, 195, 142, 50);
 
-        /*okButton = {
-            x: (width - okButtonSprite.width) / 2,
-            y: height - 200,
-            width: okButtonSprite.width,
-            height: okButtonSprite.height
-        };*/
-
-        //gameLoop();
+        gameLoop();
     };
 }
 
@@ -73,19 +108,67 @@ function canvasSetup(){
     renderingContext = canvas.getContext("2d");
 }
 
+
+function gameLoop() {
+    update();
+    render();
+    window.requestAnimationFrame(gameLoop);
+}
+
+
+
+function update() {
+    frames++;
+
+    /*if (currentState !== states.Score) {
+        foregroundPosition = (foregroundPosition - 2) % 14;
+    }
+
+    if (currentState === states.Game) {
+        rainbow.update();
+    }*/
+
+    pony.update();
+
+}
+
+
+function render() {
+    // Draw background color
+    renderingContext.fillRect(0, 0, width, height);
+
+    // Draw background sprites
+    //backgroundSprite.draw(renderingContext, 0, height - backgroundSprite.height);
+    //backgroundSprite.draw(renderingContext, backgroundSprite.width, height - backgroundSprite.height);
+
+    //corals.draw(renderingContext);
+    pony.draw(renderingContext);}
+
+    /*if (currentState === states.Score) {
+        okButtonSprite.draw(renderingContext, okButton.x, okButton.y);
+    }
+
+    // Draw foreground sprites
+    foregroundSprite.draw(renderingContext, foregroundPosition, height - foregroundSprite.height);
+    foregroundSprite.draw(renderingContext, foregroundPosition + foregroundSprite.width, height - foregroundSprite.height);
+}*/
+
+
+
+
 function onpress(evt) {
     switch (currentState) {
 
-        case states.Splash: // Start the game and update the sprite speed.
+        case states.Splash: // Start the game.
             currentState = states.Game;
             pony.jump();
             break;
 
-        case states.Game: // The game is in progress. Update sprite speed.
+        case states.Game: // The game is in progress.
             pony.jump();
             break;
 
-        case states.Score: // Change from score to splash state if event within okButton bounding box
+        case states.Score: //Character crashed
             var mouseX = evt.offsetX, mouseY = evt.offsetY;
 
             if (mouseX == null || mouseY == null) {
@@ -105,3 +188,4 @@ function onpress(evt) {
             break;
     }
 }
+
